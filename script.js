f2d3f123afd3f2136bd70a91f4bbe5685cb81c91
Anneta <anneta@Users-MacBook-Pro.local>
2023-03-28 19:22:09 +0300

    done

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const slide = document.querySelector('.slide');
 const prevButton = document.getElementById('slide-arrow-prev');
 const nextButton = document.getElementById('slide-arrow-next');
 
-nextButton.addEventListener('click', event => {
+console.log(nextButton);
+
+nextButton.addEventListener('click', () => {
   const slideWidth = slide.clientWidth;
   slidesContainer.scrollLeft += slideWidth;
 });
